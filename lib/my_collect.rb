143ad1391c023788ff6a1ab2970a131dc71e0a7f
sarah <s.j.condon01@gmail.com>
2019-09-20 02:57:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+list = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-
+def= my_collect(array)
+i = 0
+  name_collect = []
+while i < array.length
+name_collection << yield(array[i]) 
+  i += 1
+end
+  name_collection
+end
+  name_collect(list) {|i| i.split(" ").first}
